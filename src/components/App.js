340234427d641f769b41db9593b10071bbc16159
Srujana <p.srujana1356@gmail.com>
2024-07-14 22:24:59 +0530

    step

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 
 import React, {useState} from "react";
 import './../styles/App.css';
-import Step1 from './Step1';
+import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Step3 from "./Step3";
 
