608c1bd79cfe49389e57d7efc5e59fa3fd300e24
Srujana <p.srujana1356@gmail.com>
2024-07-16 12:46:14 +0530

    step

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,13 @@
-import React, { useState } from 'react';
-import "./../styles/App.css";
-import Step from './Step';
-
-
+import React, { useState } from "react";
+import './../styles/App.css';
+import Step from "./Step";
 const App = () => {
-  const [currentStep, setCurrentStep] = useState(1);
-  const [formData, setFormData] = useState({
-    first_name: '',
-    last_name: '',
-    model: '',
-    car_price: '',
-    card_info: '',
-    expiry_date: ''
-  });
-
-  const nextStep = () => {
-    setCurrentStep(currentStep + 1);
-  };
-
-  const prevStep = () => {
-    setCurrentStep(currentStep - 1);
-  };
-
-  const handleChange = (input) => (e) => {
-    setFormData({ ...formData, [input]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    
-    console.log(formData);
-  };
-
+  const [currentstep,setcurrentstep]=useState(['','hide','hide']);
   return (
-    <div className="app">
-      <div className="form-container">
-        <Step
-          currentStep={currentStep}
-          formData={formData}
-          handleChange={handleChange}
-          nextStep={nextStep}
-          prevStep={prevStep}
-          handleSubmit={handleSubmit}
-        />
-      </div>
+    <div>
+      <Step currentstep={currentstep} setcurrentstep={setcurrentstep} />
+  
     </div>
-  );
-};
-
-export default App;
-
+  )
+}
+export default App
--- src/components/Step.js
+++ src/components/Step.js
@@ -1,75 +1,75 @@
-import React from 'react';
-
-const Step = ({ currentStep, formData, handleChange, nextStep, prevStep, handleSubmit }) => {
-  const { first_name, last_name, model, car_price, card_info, expiry_date } = formData;
-
-  const renderStep = () => {
-    switch (currentStep) {
-      case 1:
-        return (
-          <div id="step1">
-            <h3>Personal Information</h3>
-            <label>First Name:</label>
-            <input type="text" id="first_name" value={first_name} onChange={handleChange('first_name')} />
-            <br />
-            <label>Last Name:</label>
-            <input type="text" id="last_name" value={last_name} onChange={handleChange('last_name')} />
-          </div>
-        );
-      case 2:
-        return (
-          <div id="step2">
-            <h3>Car Details</h3>
-            <label>Car Model:</label>
-            <input type="text" id="model" value={model} onChange={handleChange('model')} />
-            <br />
-            <label>Car Price:</label>
-            <input type="text" id="car_price" value={car_price} onChange={handleChange('car_price')} />
-          </div>
-        );
-      case 3:
-        return (
-          <div id="step3">
-            <h3>Payment Information</h3>
-            <label>Card Information:</label>
-            <input type="text" id="card_info" value={card_info} onChange={handleChange('card_info')} />
-            <br />
-            <label>Expiry Date:</label>
-            <input type="text" id="expiry_date" value={expiry_date} onChange={handleChange('expiry_date')} />
-          </div>
-        );
-      default:
-        return null;
-    }
-  };
-
-  const renderButtons = () => {
-    if (currentStep === 1) {
-      return <button type="button" className="next" onClick={nextStep}>Next</button>;
-    } else if (currentStep === 3) {
-      return (
-        <div>
-          <button type="button" className="prev" onClick={prevStep}>Previous</button>
-          <button type="button" className="submit" onClick={handleSubmit}>Submit</button>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <button type="button" className="prev" onClick={prevStep}>Previous</button>
-          <button type="button" className="next" onClick={nextStep}>Next</button>
-        </div>
-      );
-    }
-  };
-
+import React, { useState } from 'react';
+ 
+const Step = (props) => {
+    const handlenext=(step)=>{
+        if (step==0) {
+            props.setcurrentstep(['hide','','hide']);
+        }else if(step==1){
+            props.setcurrentstep(['hide','hide','']); 
+        }
+         }
+    const handleprevious=(step)=>{
+        step==1?props.setcurrentstep(['','hide','hide']):props.setcurrentstep(['hide','','hide']);
+        }
+  const [error,seterror]=useState('');
+  const [error1,seterror1]=useState('');
+        const handlecardnumber=(e)=>{
+        if (e.target.value.length!=12) {
+            seterror1('Credit card number should be exactly 12 digit long.');
+        }else{
+            seterror1('');
+        }
+        }
+        const handleexpiry=(e)=>{
+            const val=e.target.value;
+            const check=/^[0-9][0-9][/][0-9][0-9]$/.test(val);
+            console.log(check);
+            if (!check) {
+                seterror('Expiration date must be in the format MM/YY.');
+            }else{
+                seterror('');
+            }
+        }
   return (
-    <form onSubmit={handleSubmit}>
-      {renderStep()}
-      <br />
-      {renderButtons()}
-    </form>
-  );
-};
-
-export default Step;
+    <div>
+        {/* {props.currentstep[0] && */}
+         <div id='step1' className={'form '+props.currentstep[0]}>
+            <h3>Customer Details</h3>
+            <label for='first_name' >First Name:</label><br/>
+            <input type='text' id='first_name' placeholder='First Name' /><br/>
+            <label for='last_name' >Last Name:</label><br/>
+            <input type='text' id='last_name' placeholder='Last Name' /><br/>
+            {/* <input onClick={()=>handlenext(0)} type='button' value='Next' /> */}
+            <button onClick={()=>handlenext(0)} type='button'>Next</button>
+        </div>
+ 
+        {/* {props.currentstep[1] &&  */}
+        <div id='step2' className={'form '+props.currentstep[1]}>
+            <h3>Car Details</h3>
+            <label for='model'>Brand:</label><br/>
+            <input type='text' id='model' placeholder='Brand' /><br/>
+            <label for='car_price' >Model:</label><br/>
+            <input type='text' id='car_price' placeholder='Last Name' /><br/>
+            {/* <input type='button' value='Previous' onClick={()=>handleprevious(1)}/>
+            <input type='button' value='Next' onClick={()=>handlenext(1)}/> */}
+             <button type='button' onClick={()=>handleprevious(1)}>Previous</button>
+             <button type='button'  onClick={()=>handlenext(1)}>Next</button>
+        </div>
+        {/* {props.currentstep[2] && */}
+         <div id='step3' className={'form '+props.currentstep[2]}>
+            <h3>Payment Details</h3>
+            <label for='card_info' >Credit card Number:</label><br/>
+            <input onChange={handlecardnumber} type='number' id='card_info' placeholder='123456789012' /><br/>
+            <p id='error1' style={{color:'red'}}>{error1}</p>
+            <label  for='expiry_date' >Expiration Date:</label><br/>
+            <input onChange={handleexpiry} type='text' id='expiry_date' placeholder='MM/YY' /><br/>
+            <p id='error' style={{color:'red'}}>{error}</p>
+            {/* <input type='button' value='Previous' onClick={()=>handleprevious(2)} /> */}
+            <button type='button' onClick={()=>handleprevious(2)}>Previous</button>
+            <button type='submit'>Submit</button>
+        </div>
+    </div>
+  )
+}
+ 
+export default Step
